634f5f8c48a8b1c6f482bb52879fc49c82134b1d
IuriTPena <iuritavarespena@gmail.com>
2016-12-23 01:27:19 +0000

    added nr of crimes per hour

---
+++ explor.R
@@ -0,0 +1,9 @@
+#import data
+source("import.R")
+library(ggplot2)
+
+#number of crimes per period (week)
+offensas <- select(crimes, Hour, WeekDay, NrOffen) %>% group_by(offensas, WeekDay, Hour) %>% summarise(offensas, TotalOffenses=sum(NrOffen))
+offensas$WeekDay <- factor(offensas$WeekDay, levels=c("Sun","Mon","Tues","Wed","Thurs","Fri","Sat"))
+ggplot(offensas, aes(x=Hour,y=TotalOffenses)) + geom_histogram(binwidth=3,stat="identity") + facet_wrap(~ WeekDay) + ggtitle("Offenses by day of week and hour")
+  
