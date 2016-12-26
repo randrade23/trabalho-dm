adb02dfdf60b18f1df3fc16a865d9ee7a9005ce5
Rui Andrade <ruiandrade@outlook.com>
2016-12-26 03:29:14 +0000

    working on the report

--- import.R
+++ import.R
@@ -23,6 +23,9 @@ get_period <- function(x) {
 # Load the dataset
 crimes <- read.xls("crime.xls", header=TRUE, na.strings=c("", "UNK", "-"))
 
+# Remove crimes before 2015 (too old, not relevant)
+crimes <- crimes[ymd(crimes$Date) >= ymd("2015-01-01"),]
+
 # Create Period column
 crimes <- mutate(crimes, Period = get_period(Hour))
 
