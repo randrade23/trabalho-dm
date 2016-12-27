bd68f5f6266371e237a759ec7a342a2dd68a9516
IuriTPena <iuritavarespena@gmail.com>
2016-12-27 00:58:00 +0000

    added some graphs

--- explor.R
+++ explor.R
@@ -1,9 +1,37 @@
 #import data
 source("import.R")
 library(ggplot2)
+library(GGally)
 
-#number of crimes per period (week)
-offensas <- select(crimes, Hour, WeekDay, NrOffen) %>% group_by(offensas, WeekDay, Hour) %>% summarise(offensas, TotalOffenses=sum(NrOffen))
-offensas$WeekDay <- factor(offensas$WeekDay, levels=c("Sun","Mon","Tues","Wed","Thurs","Fri","Sat"))
-ggplot(offensas, aes(x=Hour,y=TotalOffenses)) + geom_histogram(binwidth=3,stat="identity") + facet_wrap(~ WeekDay) + ggtitle("Offenses by day of week and hour")
-  
+### graphs
+# crime frequency per month
+crimesmonth <- select(crimes,  Month, Day, NrOffen) %>% group_by(Month, Day) %>% summarise(TotalOffenses = sum(NrOffen))
+crimesmonth$Month <- factor(crimesmonth$Month, levels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"))
+ggplot(crimesmonth, aes(x=Day,y=TotalOffenses)) + geom_histogram(binwidth=3,stat="identity") + facet_wrap(~ Month) + ggtitle("Offenses by month and day")
+
+# crime freuqency per hour per day of week
+crimesweek <- select(crimes, Hour, WeekDay, NrOffen) %>% group_by(WeekDay, Hour) %>% summarise(TotalOffenses = sum(NrOffen))
+crimesweek$WeekDay <- factor(crimesweek$WeekDay, levels=c("Sun","Mon","Tues","Wed","Thurs","Fri","Sat"))
+ggplot(crimesweek, aes(x=Hour,y=TotalOffenses)) + geom_histogram(binwidth=3,stat="identity") + facet_wrap(~ WeekDay) + ggtitle("Offenses by day of week and hour")
+
+# crime type frequency
+crimetype <- select(crimes, OffenType, NrOffen) %>% group_by(OffenType) %>% summarise(TotalOffenses = sum(NrOffen)) %>% arrange(desc(TotalOffenses))
+ggplot(crimetype, aes(x=OffenType,y=TotalOffenses)) + geom_histogram(binwidth=3,stat="identity") + ggtitle("Offenses by type")
+
+
+# crime type frequency per hour per day of week (?)
+# crimetypes <- select(crimes, OffenType, WeekDay, NrOffen) %>% group_by(OffenType,WeekDay) %>% summarise(TO = sum(NrOffen))
+# 
+# data.frame(OffenType = crimetype$OffenType)
+# 
+# ggparcoord(crimetype,columns=2:8,groupColumn=1)
+
+#TOP 10 beats
+beats <- select(crimes, Beat, NrOffen) %>% group_by(Beat) %>% summarise(TotalOffenses = sum(NrOffen)) %>% arrange(desc(TotalOffenses)) %>% head(10)
+beats$Beat <- factor(beats$Beat, levels=c("12D10","6B60","1A20","17E10","19G10","18F20","13D20","2A50","3B10","1A50"))
+ggplot(beats, aes(x=Beat,y=TotalOffenses)) + geom_histogram(binwidth=3,stat="identity") + coord_flip() + ggtitle("TOP10 Beats")
+
+#TOP 10 premises
+premise <- select(crimes, Premise, NrOffen) %>% group_by(Premise) %>% summarise(TotalOffenses = sum(NrOffen)) %>% arrange(desc(TotalOffenses)) %>% head(10)
+premise$Premise <- factor(premise$Premise, levels=c("Residence or House","Apartment Parking Lot","Apartment","Road, Street, or Sidewalk","Restaurant or Cafeteria Parking Lot","Driveway","Department or Discount Store","Other Parking Lot","Miscellaneous Business (Non-Specific)","Grocery Store or Supermarket"))
+ggplot(premise, aes(x=Premise,y=TotalOffenses)) + geom_histogram(binwidth=3,stat="identity") + coord_flip() + ggtitle("TOP10 Premises")
