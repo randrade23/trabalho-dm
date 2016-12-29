6dd9b77edb9673c49509a4c8a6cd0d006bd1d600
Rui Andrade <ruiandrade@outlook.com>
2016-12-29 15:24:10 +0000

    fixing predictors

--- predictive.R
+++ predictive.R
@@ -6,12 +6,14 @@ library(splitstackshape)
 source("import.R")
 
 set.seed(1)
-crimes_pred <- crimes %>% expandRows("NrOffen") %>% dplyr::select(Day, OffenType, Premise, StreetName, BlockRange, Period, Beat, WeekDay)
-crimes_pred$Period <- factor(crimes_pred$Period) # Due to NA coersion 
+crimes_pred <- summarise(group_by(crimes, OffenType, Premise, WeekDay, Hour, Beat), TO=sum(NrOffen))
+crimes_pred$TO <- factor(crimes_pred$TO)
 sp <- sample(1:nrow(crimes_pred), as.integer(nrow(crimes_pred) * 0.7))
 tr <- crimes_pred[sp, ]
 ts <- crimes_pred[-sp, ]
 
-nb <- naiveBayes(Beat ~., tr, laplace=1) 
-mtrx <- table(predict(nb,ts), ts$Beat)
+# Naive Bayes
+
+nb <- naiveBayes(TO ~., tr, laplace=1) 
+mtrx <- table(predict(nb,ts), ts$TO)
 (errn <- 1-sum(diag(mtrx))/sum(mtrx))
